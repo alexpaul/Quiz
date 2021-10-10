7dea98c68d31164e40d80398f7dcf8563df0de37
alexpaul <alexpaulnyc@gmail.com>
2021-10-10 19:14:41 -0400

    quiz web application

---
+++ app.js
@@ -0,0 +1,125 @@
+const form = document.querySelector("form");
+const sumbit = document.querySelector("#submit");
+const next = document.querySelector("#next");
+
+let inputLabel = document.querySelector("#inputLabel");
+let answerInput = document.querySelector("#answerInput");
+let showAnswer = document.querySelector("#showAnswer");
+let isShowingAnswer = false;
+let questionPrompt = document.querySelector("#question");
+let message = document.querySelector("#message");
+let questionsCompleted = 0;
+let currentPrompt = {};
+let progressElement = document.querySelector("#progress");
+let progressLabel = document.querySelector("#progressLabel");
+
+const randomizeQuestions = () => {
+  for (let i = questions.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * i);
+    const temp = questions[i];
+    questions[i] = questions[j];
+    questions[j] = temp;
+  }
+};
+
+const quizCompleted = () => {
+  questionPrompt.style.display = "none";
+  inputLabel.style.display = "none";
+  answerInput.style.display = "none";
+  sumbit.style.display = "none";
+  next.style.display = "none";
+  progressElement.style.display = "none";
+  progressLabel.style.display = "none";
+  message.innerText = "Great Job completing this Quiz 🥳";
+  showAnswer.style.display = "none";
+};
+
+const fetchQuestion = () => {
+  if (questionsCompleted === questions.length) {
+    quizCompleted();
+    return; // done
+  }
+
+  currentPrompt = questions[questionsCompleted];
+
+  const question = currentPrompt.question;
+
+  questionPrompt.innerText = question;
+
+  if (currentPrompt.answer.length > 1) {
+    let span = document.createElement("span");
+    span.style.color = "#d90429";
+    span.innerText = " (Note: please add commas between your answers)";
+    questionPrompt.append(span);
+  }
+
+  questionsCompleted += 1;
+
+  message.innerText = "";
+  showAnswer.innerText = "Show Answer";
+  isShowingAnswer = false;
+};
+
+const checkAnswer = () => {
+  const userAnswerArr = answerInput.value
+    .toLowerCase()
+    .split(",")
+    .map((answer) => answer.trim());
+  const correctAnswerArr = currentPrompt.answer.map((answer) =>
+    answer.toLowerCase()
+  );
+
+  let checker = (arr, target) => target.every((v) => arr.includes(v));
+
+  const isCorrect = checker(correctAnswerArr, userAnswerArr);
+
+  isCorrect
+    ? (message.innerText = "Correct")
+    : (message.innerText = "Incorrect");
+};
+
+// start up logic
+progressElement.max = questions.length;
+
+randomizeQuestions();
+
+fetchQuestion();
+
+// action: fetch next question
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (event.key === "Enter") {
+    checkAnswer();
+    return;
+  }
+});
+
+next.addEventListener("click", () => {
+  answerInput.value = "";
+  message.innerText = "";
+  fetchQuestion();
+  progressElement.value = questionsCompleted;
+  answerInput.focus();
+});
+
+// action: answer submitted
+sumbit.addEventListener("click", () => {
+  checkAnswer();
+});
+
+form.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    checkAnswer();
+  }
+});
+
+showAnswer.addEventListener("click", () => {
+  if (!isShowingAnswer) {
+    message.innerText = currentPrompt.answer;
+    showAnswer.innerText = "Hide Answer";
+  } else {
+    message.innerText = "";
+    showAnswer.innerText = "Show Answer";
+  }
+  isShowingAnswer = !isShowingAnswer;
+});
---
+++ questions.js
@@ -0,0 +1,72 @@
+const questions = [
+    {
+        id: 1,
+        question: 'What is the space between one place and another called?',
+        answer: ['Distance']
+    },
+    {
+        id: 2,
+        question: 'What is used to find the distance between two places on a map?',
+        answer: ['Map Scale']
+    },
+    {
+        id: 3,
+        question: 'What helps us know what direction we need to go on a map?',
+        answer: ['Compass Rose']
+    },
+    {
+        id: 4,
+        question: `What's the symbol on the map stand for?`,
+        answer: ['Map Key']
+    },
+    {
+        id: 5,
+        question: 'What are the lines that go East to West called?',
+        answer: ['Latitude Lines']
+    },
+    {
+        id: 6,
+        question: 'What are the lines that go North to South called?',
+        answer: ['Longitude Lines']
+    },
+    {
+        id: 7,
+        question: 'What are real or artificial lines that separate geographic areas like countries, states, and cities called?',
+        answer: ['Borders']
+    },
+    {
+        id: 8,
+        question: 'What tells us about the map?',
+        answer: ['Title']
+    },
+    {
+        id: 9,
+        question: 'What is 0 degress latitude called?',
+        answer: ['Equator']
+    },
+    {
+        id: 10,
+        question: 'What is 0 degrees longitude called?',
+        answer: ['Prime Meridian']
+    },
+    {
+        id: 11,
+        question: 'Name the seven continents',
+        answer: ['North America', 'South America', 'Africa', 'Asia', 'Europe', 'Antarctica', 'Australia']
+    },
+    {
+        id: 12,
+        question: `Name the five borough's of New York City?`,
+        answer: ['Queens', 'Brooklyn', 'Manhattan', 'Staten Island', 'Bronx']
+    },
+    {
+        id: 13,
+        question: 'Name the Cardinal Directions?',
+        answer: ['North', 'East', 'South', 'West'] // Never Eat Soggy Waffles
+    },
+    {
+        id: 14,
+        question: 'Name the intermediate directions?',
+        answer: ['Northeast', 'Southeast', 'Southwest', 'Northwest']
+    }
+]
